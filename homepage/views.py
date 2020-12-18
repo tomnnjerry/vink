73e4ed76bbb563d65d6c61275c73db95ec283a01
Manish <75560855+tomnnjerry@users.noreply.github.com>
2020-12-18 23:03:59 +0530

    V2

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    context = {
+
+    }
+    return render(request, 'index.html', context)
